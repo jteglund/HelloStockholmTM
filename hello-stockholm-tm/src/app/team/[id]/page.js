b6bc8ea9a8634e48db1ec360d5b871c63abb9c76
Jonas <jonas.a.teglund@gmail.com>
2024-01-07 23:16:42 +0100

    Added dynamic routing

---
+++ hello-stockholm-tm/src/app/team/[id]/page.js
@@ -0,0 +1,12 @@
+import styles from '../../page.module.css'
+
+export default function Home({params}) {
+
+return (
+    <main className={styles.main}>
+        <div className={styles.center}>
+            {params.id}
+        </div>
+    </main>
+  )
+}
