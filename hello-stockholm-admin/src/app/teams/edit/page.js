febb2c4bff91b53f570ee2ef8011d4c4eb6a037c
Jonas <jonas.a.teglund@gmail.com>
2024-01-14 13:11:13 +0100

    Edit team

--- hello-stockholm-admin/src/app/teams/edit/page.js
+++ hello-stockholm-admin/src/app/teams/edit/page.js
@@ -6,36 +6,131 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../../firebase-config';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import { db } from '../../firebase-config'
+import { collection, getDocs, doc, updateDoc} from 'firebase/firestore'
 
 export default function Home() {
-    const [loggedin, setLoggedin] = useState(false);
-    const router = useRouter();
+  const [loggedin, setLoggedin] = useState(false);
+  const router = useRouter();
+  const [teamName, setTeamName] = useState("");
+  const [division, setDivision] = useState("0");
+  const [teams, setTeams] = useState(null);
+  const teamsCollectionRef = collection(db, "Team");
+  const [teamIndex, setTeamIndex] = useState(null);
+  const [editName, setEditName] = useState("");
+  const [editDivision, setEditDivision] = useState("");
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-              const uid = user.uid;
-              setLoggedin(true)
-            } else {
-              setLoggedin(false);
-              router.push('/');
-            }
-          });
-         
-    }, [])
+  const updateTeamName = (event) => {
+    setTeamName(event.target.value);
+  }
+  const updateEditName = (event) => {
+    setEditName(event.target.value);
+  }
 
-    return (
-        <main className={styles.main}>
-          { loggedin &&
-            <div className={styles.centerVert}>
-              <h2>Teams</h2>
-              <div className={styles.center}>
-                <Link href={"/loggedin"}>
-                  <div className={styles.enterButton}>Go back</div>
-                </Link>
-              </div>
-            </div>
+  const saveEdit = async () => {
+    let newDiv = parseInt(editDivision);
+    if(editName === ""){
+      return null;
+    }
+    const teamRef = doc(db, "Team", teams[teamIndex].id);
+    await updateDoc(teamRef, {
+      Name: editName,
+      Division: newDiv
+    });
+    router.push("/teams")
+  }
+
+  const checkIfTeamExists = () => {
+    if(teams){
+      for(let i in teams){
+        if(teams[i].Name === teamName && teams[i].Division === parseInt(division)){
+          setTeamIndex(i);
+          setEditName(teams[i].Name);
+          setEditDivision(teams[i].Division.toString());
+          return true;
+        }
+      }
+      setTeamIndex(null);
+      return false;
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    const getTeams = async () => {
+      const data = await getDocs(teamsCollectionRef);
+      setTeams(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    
+    getTeams();
+  }, [])
+
+  useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+          if (user) {
+            const uid = user.uid;
+            setLoggedin(true)
+          } else {
+            setLoggedin(false);
+            router.push('/');
           }
-        </main>
-  )
+        });
+        
+  }, [])
+
+  return (
+      <main className={styles.main}>
+        { loggedin && teams &&
+          <div className={styles.centerVert}>
+            <h2>Create Team</h2>
+            <div className={styles.center}>
+              <Link href={"/teams"}>
+                <div className={styles.enterButton}>Go back</div>
+              </Link>
+            </div>
+            <div className={styles.center2}> 
+              <h3>Team name: </h3>
+              <input value={teamName} onChange={updateTeamName} className={styles.input} placeholder='Enter team name'></input>
+            </div>
+            <div className={styles.center2}> 
+              <h3>Division:</h3>
+              <select 
+                value={division}
+                onChange={e => setDivision(e.target.value)}
+                className={styles.select}
+              >
+                <option value="0">Open</option>
+                <option value="1">Women</option>
+              </select>
+            </div>
+            <div className={styles.center}>
+              <div className={styles.createButton} onClick={checkIfTeamExists}>Edit team</div>
+            </div>
+            
+            { teamIndex &&
+              <>
+                <div className={styles.center2}>
+                  <h3>Edit name: </h3>
+                  <input value={editName} onChange={updateEditName} className={styles.input} placeholder='Enter new team name'></input>
+                </div>
+                <div className={styles.center2}>
+                  <h3>Edit division:</h3>
+                  <select
+                      value={editDivision}
+                      onChange={e => setEditDivision(e.target.value)}
+                      className={styles.select}
+                    >
+                    <option value="0">Open</option>
+                    <option value="1">Women</option>
+                  </select>
+                </div>
+                <div className={styles.center}>
+                  <div className={styles.createButton} onClick={saveEdit}>Save</div>
+                </div>
+              </>
+            }
+          </div>
+        }
+      </main>
+)
 }
