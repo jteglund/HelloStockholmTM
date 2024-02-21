8d66878aecb01a23ee12b602bc4c3011a9cfa2f0
Jonas <jonas.a.teglund@gmail.com>
2024-02-21 13:41:29 +0100

    FBCOnfig

---
+++ hello-stockholm-tm/src/app/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore"
+import { getAuth } from "@firebase/auth"
+
+const firebaseConfig = {
+ apiKey: process.env.API_KEY,
+ authDomain: "hellostockholm-10129.firebaseapp.com",
+ projectId: "hellostockholm-10129",
+ storageBucket: "hellostockholm-10129.appspot.com",
+ messagingSenderId: "573847371101",
+ appId: "1:573847371101:web:00de7ea4acc3e038febcb4",
+ measurementId: "G-Z8WGFVDVWK"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
